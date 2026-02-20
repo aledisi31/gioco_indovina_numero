f8d1657889d2746cf37f40ae6aa8170230d1dc46
aledisi31 <aledisi31@gmail.com>
2026-02-20 17:25:31 +0100

    Versione finale con la possibilità di rigiocare più partite

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 #Importa libreria random
 import random
 
-#genera un numero segreto
-numero_segreto = random.randint(1, 100)
-tentativo = 0
-tentativi_massimi = 5
 
-#Stampa un messaggio di benvenuto e chiedi all'utente di indovinare il numero segreto
-print("Benvenuto al gioco del numero segreto!")
-contatore = 5
+gioca_ancora = "s"
 
-#Ciclo finché l'utente non indovina il numero segreto
-while tentativo != numero_segreto and contatore > 0:
-    risposta = input("Indovina il numero segreto (tra 1 e 100): ")
-    try:
-        tentativo = int(risposta)
-        contatore -= 1
-    except ValueError:
-        #Se l'input non è un numero valido, stampa un messaggio di errore e continua il ciclo
-        print("Inserisci un numero valido, non una lettera!")
-        continue #ricomincia il ciclo dal via senza eseguire il resto del codice
+while gioca_ancora.lower() == "s":
+    #genera un numero segreto
+    numero_segreto = random.randint(1, 100)
+    tentativo = 0
+    tentativi_massimi = 5
+
+    #Stampa un messaggio di benvenuto e chiedi all'utente di indovinare il numero segreto
+    print("Benvenuto al gioco del numero segreto!")
+    contatore = tentativi_massimi
+
+    #Ciclo finché l'utente non indovina il numero segreto
+    while tentativo != numero_segreto and contatore > 0:
+        risposta = input("Indovina il numero segreto (tra 1 e 100): ")
+        try:
+            tentativo = int(risposta)
+            contatore -= 1
+        except ValueError:
+            #Se l'input non è un numero valido, stampa un messaggio di errore e continua il ciclo
+            print("Inserisci un numero valido, non una lettera!")
+            continue #ricomincia il ciclo dal via senza eseguire il resto del codice
+        
+
     
 
-  
+        if tentativo < numero_segreto:
+            print(f"Troppo basso! Riprova. Hai {contatore} tentativi rimasti.")
+        elif tentativo > numero_segreto:
+            print(f"Troppo alto! Riprova. Hai {contatore} tentativi rimasti.")
 
-    if tentativo < numero_segreto:
-        print("Troppo basso! Riprova. Hai", contatore, "tentativi rimasti.")
-    elif tentativo > numero_segreto:
-        print("Troppo alto! Riprova. Hai", contatore, "tentativi rimasti.")
+    if tentativo == numero_segreto:
+            print(f"Hai indovinato in {5 - contatore} tentativi.")
+    else:        print(f"Hai esaurito i tentativi! Il numero segreto era {numero_segreto}")
 
-if tentativo == numero_segreto:
-        print("Hai indovinato in", 5 - contatore, "tentativi.")
-else:        print("Hai esaurito i tentativi! Il numero segreto era", numero_segreto)
+    #Chiedi all'utente se vuole giocare ancora
+    gioca_ancora = input("Vuoi giocare ancora? (s/n): ")
+print("Grazie per aver giocato! Arrivederci!")
